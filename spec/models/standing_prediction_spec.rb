dae6f588570a49272b4b71c2ea40d1c44bdbb615
Dragos Calin <calinho_gx1992@yahoo.com>
2014-01-16 04:12:43 +0200

    Added Specs for the uniqueness Validations on the StandingPrediction Model

--- spec/models/standing_prediction_spec.rb
+++ spec/models/standing_prediction_spec.rb
@@ -12,7 +12,22 @@ describe StandingPrediction do
     let(:t2) { Tour.create!(name: "Tour of Flanders") }
     let(:m1) { Manager.create! }
     let(:m2) { Manager.create! }
-
     
+    before do
+      m1.standing_predictions.create!(rank: 1, tour: t1, cyclist: c1)
+    end
+    # Make sure one Rank can appear only once for each tour of each manager
+    subject { m1 }
+    it "should not be able to select a rank more than once in a tour" do
+      expect { m1.standing_predictions.create!(rank: 1, tour: t1, cyclist: c1) }.to raise_error(ActiveRecord::RecordInvalid)
+      expect { m1.standing_predictions.create!(rank: 1, tour: t1, cyclist: c2) }.to raise_error(ActiveRecord::RecordInvalid)
+    end  
+
+    # Make sure one Cyclist can appear only once for each tour of each manager
+    it "Manager should not be able to select a cyclist more than once in a tour" do
+      expect { m1.standing_predictions.create!(rank: 1, tour: t1, cyclist: c1) }.to raise_error(ActiveRecord::RecordInvalid)
+      expect { m1.standing_predictions.create!(rank: 2, tour: t1, cyclist: c1) }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
   end
 end  
